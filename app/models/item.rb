352de137046c690a91347225923e4a87f8e8256a
Muhammad Fata Nuryanto <fatanuryanto1010@gmail.com>
2022-04-22 19:44:04 +0000

    Drop db and remigrate and adding seed

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,3 @@
 class Item < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { maximum: 150 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_191810) do
+ActiveRecord::Schema.define(version: 2022_04_22_193514) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_04_22_191810) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "price"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
