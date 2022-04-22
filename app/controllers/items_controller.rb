64dcfe7299d11d3ec4fbe76662e1bc0560467fdd
Muhammad Fata Nuryanto <fatanuryanto1010@gmail.com>
2022-04-22 19:51:31 +0000

    Fixing some bug in views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,5 +13,21 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(params.require(:item).permit(:name, :price,:description))
+
+    redirect_to items_path
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+
+    redirect_to items_path
+  end
+
 end
