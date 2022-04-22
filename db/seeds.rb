ffeb2d45bcd74a8e0ef64d5f9c1d4f7c145c8d81
Muhammad Fata Nuryanto <fatanuryanto1010@gmail.com>
2022-04-22 18:29:08 +0000

    Add Customer controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Customer.create(name: "Muhammad Fata Nuryanto",phone: "088223951155",address: "Cileungsi, Kab.Bogor")
