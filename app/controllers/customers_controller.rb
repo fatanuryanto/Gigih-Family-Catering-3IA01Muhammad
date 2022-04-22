90926d0c94ef6c9dd9e405f17e6ba7d143ed2d87
Muhammad Fata Nuryanto <fatanuryanto1010@gmail.com>
2022-04-22 19:15:07 +0000

    Add configuration to Customer Controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,34 @@
 class CustomersController < ApplicationController
   def index
+    @customers=Customer.all
   end
 
   def new
+    @customer=Customer.new
+  end
+
+  def create
+    customer = Customer.create(params.require(:customer).permit(:name, :phone,:address))
+
+    redirect_to customers_path
   end
 
   def edit
+    @customer=Customer.find(params[:id])
   end
+
+  def update
+    customer=Customer.find(params[:id])
+    customer = Customer.update(params.require(:customer).permit(:name, :phone,:address))
+
+    redirect_to customers_path
+  end
+
+  def destroy
+    customer = Customer.find(params[:id])
+    customer.destroy
+
+    redirect_to customers_path
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-  get 'customers/new'
-  get 'customers/edit'
+  resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
