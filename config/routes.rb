1cea58da4e2777e166e0ea8b6584c1bb144fc6d9
Muhammad Fata Nuryanto <fatanuryanto1010@gmail.com>
2022-04-22 18:52:57 +0000

    Fixing controller Customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'customer/index'
-  get 'customer/new'
-  get 'customer/edit'
+  get 'customers/index'
+  get 'customers/new'
+  get 'customers/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,18 +1,18 @@
 require "test_helper"
 
-class CustomerControllerTest < ActionDispatch::IntegrationTest
+class CustomersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get customer_index_url
+    get customers_index_url
     assert_response :success
   end
 
   test "should get new" do
-    get customer_new_url
+    get customers_new_url
     assert_response :success
   end
 
   test "should get edit" do
-    get customer_edit_url
+    get customers_edit_url
     assert_response :success
   end
 end
